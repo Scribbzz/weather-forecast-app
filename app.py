800559a9b177a305e89ad929c2923f5c3c11f3f6
Scribbzz <ahaanmandan@gmail.com>
2025-04-29 05:59:42 -0600

    Implemented logic

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-API_KEY = "your_openweathermap_api_key_here"
+API_KEY = "1ccebaa7983a82f95adffebc39fb8f19"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
